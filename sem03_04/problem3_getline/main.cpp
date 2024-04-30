aa4dfa52b4de1578ed573bb6e5c5c818d839608b
crazyrinchik <156314414+crazyrinchik@users.noreply.github.com>
2024-04-30 21:45:51 +0300

    Create main.cpp

---
+++ sem03_04/problem3_getline/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <sstream>
+#include <cstring>
+
+int main()
+{
+    char str[100];
+    std::cin.getline(str, 100);
+    std::cout << str << std::endl;
+
+    return 0;
+}
