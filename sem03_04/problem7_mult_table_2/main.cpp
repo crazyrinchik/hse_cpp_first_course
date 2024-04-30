3b4eaad399e078d49a8c7d9085d6c6670e4eb6ed
crazyrinchik <156314414+crazyrinchik@users.noreply.github.com>
2024-04-30 21:54:22 +0300

    Create main.cpp

---
+++ sem03_04/problem7_mult_table_2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main()
+{
+    int m, n;
+    std::cin >> m >> n;
+    std::string line = "\t";
+
+    for (int l = 1; l < n+1; l++) {
+        line += std::to_string(l) + "\t";
+    }
+    
+    std::cout << line << std::endl;
+  
+    for (int i = 1; i <= m; i++) 
+    {
+        line = std::to_string(i) + "\t";
+        for (int j = 1; j < n+1; j++) 
+        {
+            int product = i * j;
+            line += std::to_string(product) + "\t";
+        }
+        std::cout << line << std::endl;
+    }
+}
