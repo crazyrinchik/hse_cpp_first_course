50b9a93823a8eea651b2d3b24f3fe9fdec714c06
crazyrinchik <156314414+crazyrinchik@users.noreply.github.com>
2024-05-04 21:36:40 +0300

    Create main.cpp

---
+++ sem09_10/problem8_append/main.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void appendToFile(const std::string& filePath, const std::string& content)
+{
+    std::ofstream outputFile(filePath, std::ios::app);
+    
+    outputFile << content;
+}
+
+int main()
+{
+    appendToFile("file.txt", "Appended content.\n");
+    return 0;
+}
